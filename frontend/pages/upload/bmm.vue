37c9ef3d22b0b6a92e6541106f33c52d896354f4
Andreas Gangsø <andreasgangso@gmail.com>
2024-06-25 15:49:10 +0200

    fix: upload

--- frontend/pages/upload/bmm.vue
+++ frontend/pages/upload/bmm.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
-import {BmmEnvironment} from "~/src/gen/api/v1/api_pb";
+import { BmmEnvironment } from "~/src/gen/api/v1/api_pb";
 
 const form = ref<BMMSingleForm>({
     title: "",
-    environment: "prod"
+    environment: "prod",
 });
 
 const metadataIsSet = ref(false);
@@ -17,9 +17,12 @@ const config = useRuntimeConfig();
 
 const selectedEnvironment = ref(BmmEnvironment.Production);
 watch(form, (newForm) => {
-  const newEnvironment = newForm.environment === "int" ? BmmEnvironment.Integration : BmmEnvironment.Production;
-  if (selectedEnvironment.value !== newEnvironment)
-    selectedEnvironment.value = newEnvironment;
+    const newEnvironment =
+        newForm.environment === "int"
+            ? BmmEnvironment.Integration
+            : BmmEnvironment.Production;
+    if (selectedEnvironment.value !== newEnvironment)
+        selectedEnvironment.value = newEnvironment;
 });
 
 const metadata = computed(() => {
@@ -32,17 +35,24 @@ const metadata = computed(() => {
 });
 
 const availableLanguages = ref<string[]>([]);
-watch([me], async([newMe])=> {
-  const newLanguages = newMe?.bmm?.languages;
-  if (newLanguages && newLanguages.length > 0){
-    availableLanguages.value = newLanguages;
-  } else {
-    availableLanguages.value = (await api.getLanguages({environment: selectedEnvironment.value})).Languages;
-  }
-}, {immediate: true});
+watch(
+    [me],
+    async ([newMe]) => {
+        const newLanguages = newMe?.bmm?.languages;
+        if (newLanguages && newLanguages.length > 0) {
+            availableLanguages.value = newLanguages;
+        } else {
+            availableLanguages.value = (
+                await api.getLanguages({
+                    environment: selectedEnvironment.value,
+                })
+            ).Languages;
+        }
+    },
+    { immediate: true },
+);
 
 const uploaded = ref(false);
-
 </script>
 
 <template>
@@ -50,47 +60,46 @@ const uploaded = ref(false);
         class="mx-auto flex min-h-screen max-w-screen-md flex-col gap-4 rounded-lg bg-stone-300 p-4 text-black"
         v-if="me && me.bmm"
     >
-      <template v-if="me.bmm && (me.bmm.podcasts.length > 0 || me.bmm.admin)">
-          <template v-if="!uploaded">
-              <BmmSingleMetadata
-                  v-model="form"
-                  @set="metadataIsSet = true"
-                  :permissions="me.bmm"
-                  :environment="selectedEnvironment"
-              />
-            <div
-                  class="flex flex-col gap-4 p-4 transition"
-                  :class="[
-                      {
-                          'pointer-events-none opacity-50': false,
-                      },
-                  ]"
-              >
-                  <h3 class="text-lg font-bold">Upload File</h3>
+        <template v-if="me.bmm && (me.bmm.podcasts.length > 0 || me.bmm.admin)">
+            <template v-if="!uploaded">
+                <BmmSingleMetadata
+                    v-model="form"
+                    @set="metadataIsSet = true"
+                    :permissions="me.bmm"
+                    :environment="selectedEnvironment"
+                />
+                <div
+                    class="flex flex-col gap-4 p-4 transition"
+                    :class="[
+                        {
+                            'pointer-events-none opacity-50': false,
+                        },
+                    ]"
+                >
+                    <h3 class="text-lg font-bold">Upload File</h3>
 
-                  <div v-for="file in selectedFiles"
-                       :key="file.name">
-
-                    {{file.name}}
-                  </div>
-                  <SelectFile v-model="selectedFiles" />
-                  <FileUploader
-                      v-model="selectedFile"
-                      :endpoint="config.public.grpcUrl + '/upload'"
-                      :metadata="metadata"
-                      @uploaded="uploaded = true"
-                  />
-                  <div>
-                    {{ metadata }}
-                  </div>
-              </div>
-          </template>
-          <template v-else>
-              <div class="rounded-lg bg-green-500 p-4">{{ $t("uploaded") }}</div>
-          </template>
-      </template>
-      <template v-else>
-          You don't have enough permissions
-      </template>
+                    <div v-for="file in selectedFiles" :key="file.name">
+                        {{ file.name }}
+                    </div>
+                    <SelectFile v-model="selectedFiles" />
+                    <FileUploader
+                        v-for="(file, i) in selectedFiles"
+                        v-model="selectedFiles[i]"
+                        :endpoint="config.public.grpcUrl + '/upload'"
+                        :metadata="metadata"
+                        @uploaded="uploaded = true"
+                    />
+                    <div>
+                        {{ metadata }}
+                    </div>
+                </div>
+            </template>
+            <template v-else>
+                <div class="rounded-lg bg-green-500 p-4">
+                    {{ $t("uploaded") }}
+                </div>
+            </template>
+        </template>
+        <template v-else> You don't have enough permissions </template>
     </div>
 </template>
