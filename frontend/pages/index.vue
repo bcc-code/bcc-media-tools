1f49220aa857e99fb2b2eeae7dc859d2e541a71b
Matjaz Debelak <matjaz.debelak@bcc.no>
2024-08-20 11:26:38 +0200

    Add trailing /

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex flex-col gap-4">
-        <NuxtLink to="/upload/bmm" class="rounded bg-slate-700 p-2"
+        <NuxtLink to="/upload/bmm/" class="rounded bg-slate-700 p-2"
             >BMM Upload</NuxtLink
         >
-        <NuxtLink to="/transcription" class="rounded bg-slate-700 p-2"
+        <NuxtLink to="/transcription/" class="rounded bg-slate-700 p-2"
             >Transcription</NuxtLink
         >
     </div>
