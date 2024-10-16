7b2bfd5392713e76bd38c1eec022d55ee9704736
Sigve Hansen <sighan12@gmail.com>
2024-10-16 11:23:01 +0200

    feat(bmm-upload): fix various css/ ux things

--- frontend/components/bmm/BmmTrackView.vue
+++ frontend/components/bmm/BmmTrackView.vue
@@ -1,39 +1,54 @@
 <script lang="ts" setup>
-import { BMMTrack } from "~/src/gen/api/v1/api_pb";
+import type { BMMTrack, Language, LanguageList } from "~/src/gen/api/v1/api_pb";
 
-defineProps<{
+const props = defineProps<{
     track: BMMTrack;
+    languages?: LanguageList;
 }>();
 
+const formatter = new Intl.DateTimeFormat("en-GB", { timeZone: "Europe/Oslo" });
 const dateString = (date: Date) => {
-    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    return formatter.format(date);
 };
+
+function trackHasLanguage(language: Language) {
+    return props.track.languages?.Languages.some(
+        (l) => l.code === language.code,
+    );
+}
+
+const { me } = useMe();
+const availableLanguages = computed(() =>
+    props.languages?.Languages.filter(
+        (l) => me.value?.bmm?.languages.includes(l.code) ?? false,
+    ),
+);
 </script>
 
 <template>
     <div
-        class="flex w-full cursor-pointer items-center overflow-clip rounded-md border border-on-primary bg-primary shadow-sm hover:border-neutral-300"
+        class="grid w-full cursor-pointer grid-cols-[auto_1fr] grid-rows-[auto_1fr] overflow-clip rounded-md border border-on-primary bg-primary shadow-sm hover:border-neutral-300"
     >
         <span
             v-if="track && track.publishedAt"
-            class="w-24 bg-secondary px-2 py-1 text-left text-secondary"
+            class="row-span-2 border-r border-on-primary px-2 py-1 text-left tabular-nums text-tertiary"
         >
             {{ dateString(track.publishedAt.toDate()) }}
         </span>
-        <span class="grow px-2 py-1 text-primary">
+        <p class="col-start-2 grow px-2 py-1 text-primary">
             {{ track.title }}
-        </span>
-        <span
-            v-if="track.languages?.Languages.length"
-            class="flex h-full gap-1 bg-secondary px-2 py-2"
+        </p>
+        <div
+            class="col-start-2 row-start-2 flex h-full flex-wrap gap-1 border-t border-on-primary px-2 py-2"
         >
             <img
-                v-for="l in track.languages.Languages"
+                v-for="l in availableLanguages"
                 :title="l.code"
                 :src="'/images/flags/' + l.iconFile"
-                class="inline h-4 rounded-sm border border-white shadow-sm"
+                class="inline h-4 rounded-sm border border-white shadow data-[disabled=true]:scale-90 data-[disabled=true]:opacity-25 data-[disabled=true]:grayscale"
                 :alt="l.code"
+                :data-disabled="!trackHasLanguage(l)"
             />
-        </span>
+        </div>
     </div>
 </template>
--- frontend/pages/upload/bmm.vue
+++ frontend/pages/upload/bmm.vue
@@ -13,7 +13,6 @@ const metadataIsSet = ref(false);
 
 const selectedFiles = ref<FileAndLanguage[]>([]);
 
-const api = useAPI();
 const { me } = useMe();
 const config = useRuntimeConfig();
 
@@ -36,25 +35,6 @@ const metadata = computed<Record<string, string[]>>(() => {
     return f;
 });
 
-const availableLanguages = ref<string[]>([]);
-watch(
-    [me],
-    async ([newMe]) => {
-        const newLanguages = newMe?.bmm?.languages;
-        if (newLanguages && newLanguages.length > 0) {
-            availableLanguages.value = newLanguages;
-        } else {
-            const languageList = await api.getLanguages({
-                environment: selectedEnvironment.value,
-            });
-            availableLanguages.value = languageList.Languages.map(
-                (l) => l.code,
-            );
-        }
-    },
-    { immediate: true },
-);
-
 const uploaded = ref(false);
 
 const reset = () => {
@@ -70,127 +50,125 @@ const reset = () => {
 </script>
 
 <template>
-    <div
-        class="mx-auto my-8 flex h-full max-w-screen-md flex-col gap-4 rounded-2xl border border-on-secondary bg-white p-4 text-black"
-    >
-        <template
-            v-if="me && me.bmm && (me.bmm.podcasts.length > 0 || me.bmm.admin)"
+    <div class="p-4 md:p-8">
+        <div
+            class="mx-auto flex h-full max-w-screen-lg flex-col gap-4 rounded-2xl border border-on-secondary bg-white p-4 text-black"
         >
-            <template v-if="!uploaded">
-                <!-- @vue-expect-error The component's `v-model` expects a form with the type `BMMSingleForm` -->
-                <BmmSingleMetadata
-                    v-if="!metadataIsSet"
-                    v-model="form"
-                    :permissions="me.bmm"
-                    :environment="selectedEnvironment"
-                    @set="metadataIsSet = true"
-                />
-
-                <div
-                    v-if="metadataIsSet && form.track"
-                    class="flex flex-col gap-4 p-4 transition"
-                >
-                    <header>
-                        <h1 class="text-heading-xl">
-                            Upload files for "{{ form.track.title }}"
-                        </h1>
-                        <p class="text-heading-md">
-                            Existing languages:
-                            <img
-                                v-for="l in form.track.languages?.Languages"
-                                :title="l.code"
-                                :src="'/images/flags/' + l.iconFile"
-                                class="ml-2 inline h-4 rounded-sm shadow-sm"
-                                :alt="l.code"
-                            />
-                        </p>
-                    </header>
-
-                    <BccTable
-                        :items="selectedFiles"
-                        :columns="[
-                            {
-                                key: 'language',
-                                text: 'Language',
-                                sortable: false,
-                            },
-                            { key: 'file.name', text: 'Name' },
-                            {
-                                key: 'actions',
-                                text: 'Actions',
-                                sortable: false,
-                            },
-                        ]"
-                    >
-                        <template #item.file.name="{ item }">
-                            <div
-                                class="max-w-[420px] truncate"
-                                :title="item.file.name"
-                            >
-                                {{ item.file.name }}
-                            </div>
-                        </template>
-                        <template #item.language="{ item }">
-                            <LanguageSelector
-                                v-model="item.language"
-                                :class="{
-                                    hidden: !me.bmm.admin,
-                                }"
-                                :disabled="!me.bmm.admin"
-                                :languages="me.bmm.languages"
-                                :env="selectedEnvironment"
-                                label=""
-                            />
-                        </template>
-                        <template #item.actions="{ item }">
-                            <BccButton
-                                @click="
-                                    selectedFiles.splice(
-                                        selectedFiles.indexOf(item as any),
-                                        1,
-                                    )
-                                "
-                                context="danger"
-                                variant="tertiary"
-                            >
-                                <Icon name="heroicons:trash" />
-                            </BccButton>
-                        </template>
-                    </BccTable>
-
-                    <SelectFile
-                        v-if="selectedFiles.length < 1 || me.bmm.admin"
-                        v-model="selectedFiles"
-                        :default-language="metadata.language[0]"
-                        :accept-multiple="me.bmm.admin"
+            <template
+                v-if="
+                    me && me.bmm && (me.bmm.podcasts.length > 0 || me.bmm.admin)
+                "
+            >
+                <template v-if="!uploaded">
+                    <!-- @vue-expect-error The component's `v-model` expects a form with the type `BMMSingleForm` -->
+                    <BmmSingleMetadata
+                        v-if="!metadataIsSet"
+                        v-model="form"
+                        :permissions="me.bmm"
+                        :environment="selectedEnvironment"
+                        @set="metadataIsSet = true"
                     />
-
-                    <FileUploader
-                        v-model="selectedFiles"
-                        :endpoint="config.public.grpcUrl + '/upload'"
-                        :metadata="metadata"
-                        @uploaded="uploaded = true"
-                    />
-
-                    <BccButton
-                        variant="secondary"
-                        @click="metadataIsSet = false"
+                    <div
+                        v-if="metadataIsSet && form.track"
+                        class="flex flex-col gap-4 p-4 transition"
                     >
-                        Back
+                        <header>
+                            <h1 class="text-heading-xl">
+                                Upload files for "{{ form.track.title }}"
+                            </h1>
+                            <p class="text-heading-md">
+                                Existing languages:
+                                <img
+                                    v-for="l in form.track.languages?.Languages"
+                                    :title="l.code"
+                                    :src="'/images/flags/' + l.iconFile"
+                                    class="ml-2 inline h-4 rounded-sm shadow-sm"
+                                    :alt="l.code"
+                                />
+                            </p>
+                        </header>
+                        <BccTable
+                            :items="selectedFiles"
+                            :columns="[
+                                {
+                                    key: 'language',
+                                    text: 'Language',
+                                    sortable: false,
+                                },
+                                { key: 'file.name', text: 'Name' },
+                                {
+                                    key: 'actions',
+                                    text: 'Actions',
+                                    sortable: false,
+                                },
+                            ]"
+                        >
+                            <template #item.file.name="{ item }">
+                                <div
+                                    class="max-w-[420px] truncate"
+                                    :title="item.file.name"
+                                >
+                                    {{ item.file.name }}
+                                </div>
+                            </template>
+                            <template #item.language="{ item }">
+                                <LanguageSelector
+                                    v-model="item.language"
+                                    :class="{
+                                        hidden: !me.bmm.admin,
+                                    }"
+                                    :disabled="!me.bmm.admin"
+                                    :languages="me.bmm.languages"
+                                    :env="selectedEnvironment"
+                                    label=""
+                                />
+                            </template>
+                            <template #item.actions="{ item }">
+                                <BccButton
+                                    @click="
+                                        selectedFiles.splice(
+                                            selectedFiles.indexOf(item as any),
+                                            1,
+                                        )
+                                    "
+                                    context="danger"
+                                    variant="tertiary"
+                                >
+                                    <Icon name="heroicons:trash" />
+                                </BccButton>
+                            </template>
+                        </BccTable>
+                        <SelectFile
+                            v-if="selectedFiles.length < 1 || me.bmm.admin"
+                            v-model="selectedFiles"
+                            :default-language="metadata.language[0]"
+                            :accept-multiple="me.bmm.admin"
+                        />
+                        <FileUploader
+                            v-model="selectedFiles"
+                            :endpoint="config.public.grpcUrl + '/upload'"
+                            :metadata="metadata"
+                            @uploaded="uploaded = true"
+                        />
+                        <BccButton
+                            variant="secondary"
+                            @click="metadataIsSet = false"
+                        >
+                            Back
+                        </BccButton>
+                    </div>
+                </template>
+                <template v-else>
+                    <BccAlert context="success">
+                        {{ $t("uploaded") }}
+                    </BccAlert>
+                    <BccButton variant="secondary" @click="reset">
+                        Upload more
                     </BccButton>
-                </div>
+                </template>
             </template>
-
-            <template v-else>
-                <BccAlert context="success">
-                    {{ $t("uploaded") }}
-                </BccAlert>
-                <BccButton variant="secondary" @click="reset">
-                    Upload more
-                </BccButton>
-            </template>
-        </template>
-        <template v-else-if="permissionsLoading">Loading...</template>
-        <template v-else> You don't have enough permissions </template>
+            <template v-else-if="permissionsLoading">Loading...</template>
+            <template v-else> You don't have enough permissions </template>
+        </div>
     </div>
 </template>
