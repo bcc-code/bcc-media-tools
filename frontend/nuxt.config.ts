3870f1d1e7414f3737cb7692228f2a02d0d281b5
Sigve Hansen <sighan12@gmail.com>
2025-03-31 23:25:38 +0200

    feat: improve UX of admin page

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ["@nuxt/icon", "@nuxtjs/i18n", "vue-sonner/nuxt"],
+    modules: ["@nuxt/icon", "@nuxtjs/i18n", "vue-sonner/nuxt", "motion-v/nuxt"],
     typescript: {
         shim: false,
     },
