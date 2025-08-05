c3e80b4f7167d2dbf06b7941df50d2dba440266d
Sigve Hansen <sighan12@gmail.com>
2025-08-05 11:10:59 +0200

    fix(upload): change default from podcasts to albums

--- frontend/components/AlbumSelector.vue
+++ frontend/components/AlbumSelector.vue
@@ -17,7 +17,7 @@ const years = ref<{ [key: string]: BMMYear }>();
 const albums = ref<{ [key: string]: string }>({});
 
 const currentYear = new Date().getFullYear();
-const selectedType = ref("podcasts");
+const selectedType = ref<"podcasts" | "albums">("albums");
 const selectedYear = ref(currentYear.toString());
 const albumId = defineModel<string>();
 
@@ -38,7 +38,10 @@ watch(
             await api.getAlbums({ year: parseInt(newYear), environment: env })
         ).albums;
         for (let a in albumsRes) {
-            albums.value[albumsRes[a].id] = albumsRes[a].title;
+            const alb = albumsRes[a];
+            if (alb) {
+                albums.value[alb.id] = alb.title;
+            }
         }
     },
     { immediate: true },
@@ -60,8 +63,8 @@ watch(
         v-model="selectedType"
         :label="$t('Type')"
     >
-        <option value="podcasts">{{ $t("podcasts") }}</option>
         <option value="albums">{{ $t("albums") }}</option>
+        <option value="podcasts">{{ $t("podcasts") }}</option>
     </BccSelect>
 
     <BccSelect
