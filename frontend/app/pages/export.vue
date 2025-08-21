b1eb5fc42581640da63425d513bf3bb49bf99639
Sigve Hansen <sighan12@gmail.com>
2025-08-21 11:51:20 +0200

    chore: simplify export page

--- frontend/app/pages/export.vue
+++ frontend/app/pages/export.vue
@@ -6,26 +6,33 @@ useHead({
 type Trigger = {
     id: string;
     name: string;
+    url: string;
     description?: string;
 };
 
 const triggers: Trigger[] = [
-    { id: "vx-export", name: "VX Export", description: "Export to VX" },
-    { id: "vb-export", name: "VB Export", description: "Export to VB" },
     {
-        id: "isilon-export",
-        name: "Isilon Export",
-        description: "Export to Isilon",
+        id: "sync",
+        name: "Audio Sync",
+        description: "Adjusts related audio with the specified adjustment",
+        url: "https://export.bcc.media/ingest-fix/sync",
+    },
+    {
+        id: "ingest-fix",
+        name: "Audio Extraction",
+        description: "Extract Audio from MU1 and MU2",
+        url: "https://export.bcc.media/ingest-fix",
     },
     {
         id: "bulk-shorts-export",
         name: "Bulk Shorts Export",
-        description: "Export to Bulk Shorts",
+        url: "https://export.bcc.media/bulk-shorts-export",
     },
     {
         id: "history",
         name: "Workflow History",
-        description: "Workflow History",
+        description: "A list of recent workflow runs",
+        url: "https://export.bcc.media/list",
     },
 ];
 </script>
@@ -40,10 +47,14 @@ const triggers: Trigger[] = [
             <NuxtLink
                 v-for="trigger in triggers"
                 :key="trigger.id"
-                :to="`/export/${trigger.id}`"
+                :to="trigger.url"
+                external
             >
                 <UCard>
-                    <p>{{ trigger.name }}</p>
+                    <div class="flex items-center justify-between gap-2">
+                        <p>{{ trigger.name }}</p>
+                        <Icon name="heroicons:arrow-right" class="text-muted" />
+                    </div>
                     <p v-if="trigger.description" class="text-dimmed text-sm">
                         {{ trigger.description }}
                     </p>
--- frontend/pages/export/vx-export.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="mx-auto my-8 w-full max-w-2xl p-4">
-        <h1 class="text-2xl font-bold">VX Export</h1>
-    </div>
-</template>
